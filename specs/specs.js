6ebcf8fb9ebe8af0ca5f9e56be482245be2a2ed2
diane <diane.douglas1@gmail.com>
2015-09-14 10:23:13 -0700

    Add Diane comments to specs.

--- specs/specs.js
+++ specs/specs.js
@@ -3,6 +3,13 @@ describe("findReplace", function() {
         expect(findReplace("Hello", "Hello")).to.equal(true);
     });
 
+    // it("takes blank input and returns an error.", function(){
+    //     expect(findReplace("Hello", "", "blah")).to.equal("Error!");
+    // });
+
+    //single letters
+    // 'a', 'a', 'b' should output 'b'
+
     it("will take 3 words from user: 1. to search, 2. to find, 3. to replace. will replace the stringToSearch with replaceString if stringToSearch ==== findString", function(){
         expect(findReplace("Hello", "Hello", "Goodbye")).to.equal("Goodbye");
     });
@@ -14,4 +21,15 @@ describe("findReplace", function() {
     it("Will keep punctuation while still able to change given word", function(){
         expect(findReplace("This, test, has, a lot, of unnecessary, commas!", "commas", "punctuation")).to.equal("This, test, has, a lot, of unnecessary, punctuation!");
     });
+
+
+// It will replace one letter with a different letter.
+// It will replace one word with a different full word.
+// It will replace one word in a full sentence with another word.
+// It will replace multiple instances of one word with a different word.
+//exceptions
+// It will replace complete matches only. 'he' does not match 'hello'
+// It will replace words that include punctuation if they match.
+// It will include different capitalization as a match.
+// It will not change the sentence if the search word is not found.
 });
